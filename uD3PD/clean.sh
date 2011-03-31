71d6d88900724283492f4dfd98a9adb919010bf0
Jérôme ODIER <jodier@localhost.localdomain>
2011-03-31 19:25:43 +0200

    =update

--- uD3PD/clean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-rm -f Generated_amdb_simrec_Pool
-rm -f PoolFileCatalog.xml
-rm -f PoolFileCatalog.xml.BAK
-rm -f athfile-cache.ascii
-rm -f output.root
-
---
+++ uD3PD/uD3PD.h
@@ -0,0 +1,32 @@
+/*-------------------------------------------------------------------------*/
+
+#ifndef __UD3PD_H
+#define __UD3PD_H
+
+/*-------------------------------------------------------------------------*/
+
+#include "TrkVertexFitterInterfaces/ITrackToVertexIPEstimator.h"
+
+/*-------------------------------------------------------------------------*/
+
+namespace Trk
+{
+
+	double ImpactParametersAndSigma__getD0(struct ImpactParametersAndSigma *p)
+	{
+		return p->IPd0;
+	}
+
+	double ImpactParametersAndSigma__getSigma(struct ImpactParametersAndSigma *p)
+	{
+		return p->sigmad0;
+	}
+
+};
+
+/*-------------------------------------------------------------------------*/
+
+#endif
+
+/*-------------------------------------------------------------------------*/
+
